bd560ffae51a316817074ced2bf70e7342a29456
ldz <1823799296@qq.com>
2024-09-05 17:10:59 +0800

    🎈 perf: 项目结构修改，npm脚本修改

---
+++ scripts/createProjectJson.cjs
@@ -0,0 +1,30 @@
+const fs = require("fs-extra");
+const { resolve } = require("path");
+const { execSync } = require("child_process");
+
+// 过滤出文件夹
+const projectDirs = fs.readdirSync(resolve(__dirname, "../project"));
+
+// 生成项目列表
+const projectList = projectDirs.map((dir, index) => {
+  const packageJson = fs.readJsonSync(
+    resolve(__dirname, `../project/${dir}/package.json`),
+    {
+      throws: false,
+    },
+  );
+
+  return {
+    name: packageJson.name || dir,
+    value: packageJson.name || dir,
+    key: index + 1,
+    description: packageJson.description || dir,
+  };
+});
+
+// 写入文件
+fs.writeJsonSync("./project.json", projectList, { spaces: 2 });
+
+execSync("npm run prettier", {
+  stdio: "inherit",
+});
