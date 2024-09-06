9a0cb5ad93614e8bc97a64a42bcb11a1e5f6e5a0
ldz <1823799296@qq.com>
2024-09-06 15:33:24 +0800

    ✨ feat: 新增 vue3-js-template 模板

---
+++ template/vue3-js-template/README.md
@@ -0,0 +1,5 @@
+# 项目启动步骤
+
+1. `pnpm i`
+2. `pnpm prepare`
+3. `pnpm dev`
---
+++ template/vue3-js-template/lint-staged.config.js
@@ -0,0 +1,4 @@
+export default {
+  '*.{js,jsx,vue}': ['eslint --fix', 'prettier --write'],
+  '*.json': ['prettier --write']
+}
