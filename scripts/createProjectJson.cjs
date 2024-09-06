fb1dcaf62108cfef4cf736ed6cc9ad404bd6d915
ldz <1823799296@qq.com>
2024-09-06 09:18:10 +0800

    🦄 refactor: 项目结构修改

--- scripts/createProjectJson.cjs
+++ scripts/createProjectJson.cjs
@@ -3,12 +3,12 @@ const { resolve } = require("path");
 const { execSync } = require("child_process");
 
 // 过滤出文件夹
-const projectDirs = fs.readdirSync(resolve(__dirname, "../project"));
+const projectDirs = fs.readdirSync(resolve(__dirname, "../template"));
 
 // 生成项目列表
 const projectList = projectDirs.map((dir, index) => {
   const packageJson = fs.readJsonSync(
-    resolve(__dirname, `../project/${dir}/package.json`),
+    resolve(__dirname, `../template/${dir}/package.json`),
     {
       throws: false,
     },
